93d74587440a802d878bb26c318e8dd391c1670c
Kalana Wijesekara <kalanaw@99x.lk>
2020-12-03 22:41:24 +0530

    feat(nav): add go back from details

--- App.js
+++ App.js
@@ -14,11 +14,14 @@ function HomeScreen({ navigation }) {
     </View>
   );
 }
-
-function DetailsScreen() {
+function DetailsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Details Screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
     </View>
   );
 }
