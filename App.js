488cee140d8de38c50304de70163783f2d1cc7cb
Kalana Wijesekara <kalanaw@99x.lk>
2020-12-03 21:42:46 +0530

    feat(navigation):add detail screen

--- App.js
+++ App.js
@@ -1,26 +1,36 @@
-import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const App = () => {
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <View
-          style={{
-            flexDirection: "row",
-            height: 100,
-            padding: 20
-          }}
-        >
-          <View style={{ backgroundColor: "blue", flex: 0.3 }} />
-          <View style={{ backgroundColor: "red", flex: 0.5 }} />
-          <Text>Hello World!</Text>
-        </View>
-      </SafeAreaView>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-};
+}
 
 export default App;
+
